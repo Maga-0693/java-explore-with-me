a8b0b030cb2d1130ac80de8cfe33657ab57e07b6
Magomed Keligov <maga0693l@yandex.ru>
2025-10-28 12:16:18 +0300

    Version 1

---
+++ ewm-client/src/main/java/ru/practicum/GatewayApp.java
@@ -0,0 +1,14 @@
+package ru.practicum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+
+@SpringBootApplication
+@EnableCaching
+public class GatewayApp {
+    public static void main(String[] args) {
+
+        SpringApplication.run(GatewayApp.class, args);
+    }
+}

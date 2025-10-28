b97699471264a475cf887c853bbf29124a8b2e5b
Magomed Keligov <maga0693l@yandex.ru>
2025-10-28 12:34:56 +0300

    Version 1.3

--- ewm-client/src/main/java/ru/practicum/base/BaseWebClient.java
+++ ewm-client/src/main/java/ru/practicum/base/BaseWebClient.java
@@ -4,6 +4,7 @@ import org.springframework.web.reactive.function.client.WebClient;
 
 public class BaseWebClient {
     protected final WebClient webClient;
+
     public BaseWebClient(String baseUrl, String apiPrefix) {
         this.webClient = WebClient.builder()
                 .baseUrl(baseUrl + apiPrefix)

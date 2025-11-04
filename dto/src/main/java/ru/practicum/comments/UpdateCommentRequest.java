90b179e272d44bf07c0e1fb11bbecb2f5c5f4775
Magomed Keligov <maga0693l@yandex.ru>
2025-11-04 13:33:07 +0300

    Этап 3. Version 1.12

--- dto/src/main/java/ru/practicum/comments/UpdateCommentRequest.java
+++ dto/src/main/java/ru/practicum/comments/UpdateCommentRequest.java
@@ -1,15 +1,18 @@
 package ru.practicum.comments;
 
 import jakarta.validation.constraints.NotBlank;
+import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Value;
+import lombok.experimental.FieldDefaults;
 import lombok.extern.jackson.Jacksonized;
 
 @Value
 @Builder
 @Jacksonized
+@FieldDefaults(makeFinal = true, level = AccessLevel.PRIVATE)
 public class UpdateCommentRequest {
 
     @NotBlank
-    private String text;
+    String text;
 }
